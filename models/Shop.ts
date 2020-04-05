6fa8596b4de6aac3c3dafd01d71adc69b7e15c03
tlahmann <tobiaslahmann@gmail.com>
2020-04-06 00:02:23 +0200

    Add shop stock aggregation

--- models/Shop.ts
+++ models/Shop.ts
@@ -1,9 +1,11 @@
 import { Schema, model, Model, Document } from 'mongoose';
+import mongoose from 'mongoose';
 
 interface IShop extends Document {
   name: string;
   location: any;
   products: any[];
+  stock: any[];
   address: any;
 }
 interface IShopModel extends Model<IShop> {
@@ -35,9 +37,65 @@ var ShopSchema = new Schema({
     }
   },
   products: [{ type: Schema.Types.ObjectId, ref: 'Product' }],
+  stock: [{
+    product: { type: Schema.Types.ObjectId, ref: 'Product' },
+    quantity: { type: Number },
+    updatedAt: { type: Date, default: Date.now }
+  }],
   address: { type: Schema.Types.ObjectId, ref: 'Address' },
 }, { timestamps: true, usePushEach: true });
 
 ShopSchema.index({ location: "2dsphere" });
+
+ShopSchema.post('find', softDeleteMiddleware);
+ShopSchema.post('findOne', softDeleteMiddleware);
+
+const ObjectId = mongoose.Types.ObjectId;
+
+function softDeleteMiddleware(document, next) {
+  // If `isDeleted` is not set on the query, set it to `false` so we only
+  // get docs that haven't been deleted by default
+  var _that = this;
+  // console.log(document.stock)
+  model<IShop, IShopModel>('Shop', ShopSchema).aggregate([
+    { $match: { _id: ObjectId(_that.getQuery()._id) } },
+    { $unwind: "$stock" },
+    {
+      $group: {
+        _id: "$stock.product",
+        numerator: {
+          $sum: {
+            // e ^ ( -x / 4 )
+            $multiply: [
+              {
+                $exp: {
+                  // Stunde durch delta Zeit: "1/x" oder so ähnlich
+                  $divide: [
+                    // delta TIME
+                    { $divide: [{ $subtract: ["$stock.updatedAt", new Date()] }, 3600000] },
+                    7
+                  ]
+                }
+              },
+              "$stock.quantity"
+            ]
+          }
+        },
+        denominator: { $sum: 1 },
+        timestamp: { $max: "$stock.updatedAt" }
+      }
+    },
+    {
+      $project: {
+        product: "$_id",
+        quantity: { $divide: ["$numerator", "$denominator"] },
+        updatedAt: "$timestamp"
+      }
+    }
+  ], (_, data) => {
+    document.stock = data;
+    next()
+  });
+}
 
 export default model<IShop, IShopModel>('Shop', ShopSchema);
